3911f23675e488100110e5deafb1c3c35ba290f6
manish-devop <manish96385@gmail.com>
2021-03-27 09:23:57 +0530

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+//document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+
+function onDeviceReady() {
+//     Cordova is now initialized. Have fun!
+//  //  cordova.InAppBrowser.open('https://jnctbhopal.ac.in/','_blank','location=no , zoom=no ,toolbar=no');
+
+//    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+//    document.getElementById('deviceready').classList.add('ready');
+
+    // document.getElementById('version').innerHTML = device.cordova;
+    // document.getElementById('model').innerHTML = device.model;
+    // document.getElementById('platform').innerHTML = device.platform;
+    // document.getElementById('uid').innerHTML = device.uuid;
+    // document.getElementById('androidversion').innerHTML = device.version;
+    // document.getElementById('manufacture').innerHTML = device.manufacturer;
+    // document.getElementById('isvirtual').innerHTML = device.isVirtual;
+    // document.getElementById('serialNo').innerHTML = device.serial;
+
+
+  
+
+}
